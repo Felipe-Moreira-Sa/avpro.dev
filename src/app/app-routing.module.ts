0bba23e21f46542a799dc44385b1f47a7f3fb51d
Felipe-Moreira-Sa <CACOR.2018121TADS0024@aluno.ifpi.edu.br>
2024-03-06 19:36:42 -0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { PlansComponent } from './pages/plans/plans.component';
+import { UsersComponent } from './pages/users/users.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'plans', component: PlansComponent },
+  { path: 'users', component: UsersComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,31 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { TitleBarComponent } from './title-bar/title-bar.component';
+import { SideBarComponent } from './side-bar/side-bar.component';
+import { ContentAreaComponent } from './content-area/content-area.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { PlansComponent } from './pages/plans/plans.component';
+import { UsersComponent } from './pages/users/users.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TitleBarComponent,
+    SideBarComponent,
+    ContentAreaComponent,
+    DashboardComponent,
+    PlansComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [
-    provideClientHydration()
+    provideClientHydration(),
+    provideAnimationsAsync()
   ],
   bootstrap: [AppComponent]
 })
